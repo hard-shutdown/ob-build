190849d469a654640d6738121926c68568d5edd5
Cody <59001842+hard-shutdown@users.noreply.github.com>
2022-07-30 11:59:44 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,5 +3,7 @@
 git clone https://github.com/OnionBrowser/OnionBrowser.git
 cd OnionBrowser
 git checkout 2.X
-pod repo update
+pod repo remove master
+git clone https://github.com/CocoaPods/Specs.git ~/.cocoapods/repos/master
+pod setup
 pod install
